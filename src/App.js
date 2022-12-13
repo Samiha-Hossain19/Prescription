7f54a0d4cd7a54e93bb556e63beaa91308ffe3ef
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2022-12-13 20:43:52 +0600

    axios update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Add from './Components/User/Add';
+import Edit from './Components/User/Edit';
+import Users from './Components/User/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route  path="/" exact element={<Home/>} />
+        <Route  path="/home" exact element={<Home/>} />
+        <Route  path="/users/:id" exact element={<Users/>} />
+        <Route  path="/add-user" exact element={<Add/>} />
+        <Route  path="/edit-user/:id" exact element={<Edit/>} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App
